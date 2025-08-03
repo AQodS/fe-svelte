c7b2affda60a669ef0dc88f48797b729b58e59cf
AQodS <qoddri@gmail.com>
2025-08-04 00:56:16 +0700

    feat: middleware & home views

---
+++ src/hooks.server.js
@@ -0,0 +1,21 @@
+import { redirect } from "@sveltejs/kit";
+
+export async function handle({ event, resolve }) {
+  // get cookie 'token'
+  const token = event.cookies.get("token");
+  const { pathname } = event.url;
+  const publicPaths = ["/login", "/register"];
+  const isAdminPath = pathname.startsWith("/admin");
+
+  // If the user is login already and trying to access login or redister route, redirect to dashboard
+  if (token && publicPaths.includes(pathname)) {
+    throw redirect(302, "/dashboard");
+  }
+
+  // If the user is not logged in and trying to access admin route, redirect to login
+  if (!token && isAdminPath) {
+    throw redirect(302, "/login");
+  }
+
+  return await resolve(event);
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="p-5 mb-4 bg-light rounded-3 shadow-sm">
+  <div class="container-fluid py-5">
+    <h1 class="display-5 fw-bold">Fullstack Svelte-Express</h1>
+    <p class="col-md-12 fs-4">
+      Learn fullstack with Svelte Frontend and Express Backend
+    </p>
+    <hr />
+    <a href="/register" class="btn btn-primary btn-lg me-3">Register</a>
+    <a href="/login" class="btn btn-secondary btn-lg">Login</a>
+  </div>
+</div>
